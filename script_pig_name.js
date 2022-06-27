fbae71996beda6c835467890d0ad295ea9228f9d
Darshan Kagathara <107626828+Darshan-Weybee@users.noreply.github.com>
2022-06-27 23:17:19 +0530

    Add files via upload

--- script_pig_name.js
+++ script_pig_name.js
@@ -3,12 +3,16 @@ const score1El = document.getElementById("score--1");
 const diceEl = document.querySelector(".dice");
 const current0El = document.querySelector("#current--0");
 const current1El = document.querySelector("#current--1");
+const player0El = document.querySelector(".player--0");
+const player1El = document.querySelector(".player--1");
 
 const btnNew = document.querySelector(".btn--new");
 const btnRoll = document.querySelector(".btn--roll");
 const btnHold = document.querySelector(".btn--hold");
 
+let score1 = [0, 0]; 
 let currentScore = 0;
+let activePlayer = 0;
 
 score0El.textContent = 0;
 score1El.textContent = 0;
@@ -23,9 +27,21 @@ btnRoll.addEventListener("click", function(){
 
     if(dice != 1){
         currentScore += dice;
-        current0El.textContent = currentScore;
+        document.querySelector(`#current--${activePlayer}`).textContent = currentScore;
     }
     else{
-        
+        document.querySelector(`#current--${activePlayer}`).textContent = 0;
+        currentScore = 0;
+        // document.querySelector(`.player--${activePlayer}`).classList.remove("player--active");
+        activePlayer = activePlayer == 0 ? 1 : 0;
+        // document.querySelector(`.player--${activePlayer}`).classList.add("player--active");
+        player0El.classList.toggle("player--active");
+        player1El.classList.toggle("player--active");
     }
+});
+
+btnHold.addEventListener("click", function(){
+    document.querySelector(`#score--${activePlayer}`).textContent =  Number(document.querySelector(`#score--${activePlayer}`).textContent) + Number(document.querySelector(`#current--${activePlayer}`).textContent);
+    currentScore = 0;
+    document.querySelector(`#current--${activePlayer}`).textContent = 0;
 });
