5c5d1cf5ee46510756abcfded1c9db700a640f4d
Darshan Kagathara <darshan@webtheuniverse.com>
2022-07-04 14:32:06 +0530

    Updated

--- Extra_Assignment_pig_game/pig_game.js
+++ Extra_Assignment_pig_game/pig_game.js
@@ -16,6 +16,7 @@ let activePlayer = 0;
 let scoreDecrease = 0;
 let playing = true;
 let winningScore = 10;
+let presentPlayerScore, anotherPlayerScore;
 
 score0El.textContent = 0;
 score1El.textContent = 0;
@@ -24,31 +25,41 @@ diceEl.classList.add("hidden");
 btnRoll.addEventListener("click", function () {
     if (playing) {
         const dice = Math.trunc(Math.random() * 6) + 1;
-        console.log(dice);
+        // console.log(dice);
 
         diceEl.classList.remove("hidden");
         diceEl.src = `dice-${dice}.png`;
 
         if (dice != 1) {
-            currentScore = 0;
+
             currentScore += dice;
-            scoreDecrease += currentScore;
+            // scoreDecrease += currentScore;
             document.querySelector(`#current--${activePlayer}`).textContent = currentScore;
-            document.querySelector(`#score--${activePlayer}`).textContent = Number(document.querySelector(`#score--${activePlayer}`).textContent) + currentScore;
-            if (document.querySelector(`#score--${activePlayer}`).textContent >= winningScore) {
+            if(Number(currentScore) +  Number(document.querySelector(`#score--${activePlayer}`).textContent) >= winningScore){
+                document.querySelector(`#score--${activePlayer}`).textContent = Number(currentScore) +  Number(document.querySelector(`#score--${activePlayer}`).textContent);
+                document.querySelector(`#current--${activePlayer}`).textContent = 0;
+                slider();
                 playing = false;
                 diceEl.classList.add("hidden");
                 document.querySelector(`.player--${activePlayer}`).classList.add("player--winner");
                 document.querySelector(`.player--${activePlayer}`).classList.remove("player--active");
             }
-            slider();
+
+            // document.querySelector(`#score--${activePlayer}`).textContent = Number(document.querySelector(`#score--${activePlayer}`).textContent) + currentScore;
+            // if (document.querySelector(`#score--${activePlayer}`).textContent >= winningScore) {
+            //     playing = false;
+            //     diceEl.classList.add("hidden");
+            //     document.querySelector(`.player--${activePlayer}`).classList.add("player--winner");
+            //     document.querySelector(`.player--${activePlayer}`).classList.remove("player--active");
+            // }
+            // slider();
         }
         else {
-            document.querySelector(`#score--${activePlayer}`).textContent = Number(document.querySelector(`#score--${activePlayer}`).textContent) - scoreDecrease;
+            // document.querySelector(`#score--${activePlayer}`).textContent = Number(document.querySelector(`#score--${activePlayer}`).textContent) - scoreDecrease;
             document.querySelector(`#current--${activePlayer}`).textContent = 0;
             currentScore = 0;
-            scoreDecrease = 0;
-            slider();
+            // scoreDecrease = 0;
+            // slider();
             // document.querySelector(`.player--${activePlayer}`).classList.remove("player--active");
             activePlayer = activePlayer == 0 ? 1 : 0;
             // document.querySelector(`.player--${activePlayer}`).classList.add("player--active");
@@ -62,8 +73,9 @@ btnHold.addEventListener("click", function () {
     if (playing) {
         // scores[activePlayer]  += currentScore;
         // document.querySelector(`#score--${activePlayer}`).textContent = scores[activePlayer];
-        // document.querySelector(`#score--${activePlayer}`).textContent = Number(document.querySelector(`#score--${activePlayer}`).textContent) + Number(document.querySelector(`#current--${activePlayer}`).textContent);
+        document.querySelector(`#score--${activePlayer}`).textContent = Number(document.querySelector(`#score--${activePlayer}`).textContent) + Number(document.querySelector(`#current--${activePlayer}`).textContent);
         let scoreOfPlayer = document.querySelector(`#score--${activePlayer}`).textContent;
+        slider();
         if (scoreOfPlayer >= winningScore) {
             playing = false;
             diceEl.classList.add("hidden");
@@ -74,18 +86,47 @@ btnHold.addEventListener("click", function () {
             currentScore = 0;
             scoreDecrease = 0;
             document.querySelector(`#current--${activePlayer}`).textContent = 0;
-
+            
             activePlayer = activePlayer == 0 ? 1 : 0;
             player0El.classList.toggle("player--active");
             player1El.classList.toggle("player--active");
         }
     }
 });
-let winner, winnerSCore;
+let winner, winnerName;
+// let presentPlayerName, anotherPlayerName;
+
+// function getActivePlayerName(){
+// presentPlayerName = document.querySelector(`.name-${activePlayer}`).textContent;
+// console.log(presentPlayerName);
+// anotherPlayerName = document.querySelector(`.name-${activePlayer == 0 ? 1 : 0}`).textContent;
+// console.log(anotherPlayerName);
+// }
+
+// function getActivePlayerScore(){
+// presentPlayerScore = document.querySelector(`#score--${activePlayer}`).textContent;
+// console.log(presentPlayerScore);
+// anotherPlayerScore = document.querySelector(`#score--${activePlayer == 0 ? 1 : 0}`).textContent;
+// console.log(anotherPlayerScore);
+// }
+
 btnNew.addEventListener("click", function () {
-    winner = prompt("Enter winner name");
-    winnerSCore = winner == document.querySelector(`.name-${activePlayer}`).textContent ? score0El.textContent : score1El.textContent;
-    if(winnerSCore >= winningScore) {
+
+    winner = prompt("Enter lead player name");
+    let ps = document.querySelector(`#score--${activePlayer}`).textContent;
+    let as = document.querySelector(`#score--${activePlayer == 0 ? 1 : 0}`).textContent;
+    console.log(ps);
+    console.log(as);
+    // winnerSCore = winner == document.querySelector(`.name-${activePlayer}`).textContent ? score0El.textContent : score1El.textContent;
+    winnerName = Number(document.querySelector(`#score--${activePlayer}`).textContent) > Number(document.querySelector(`#score--${activePlayer == 0 ? 1 : 0}`).textContent) ? document.querySelector(`.name-${activePlayer}`).textContent : document.querySelector(`.name-${activePlayer == 0 ? 1 : 0}`).textContent;
+    console.log({winnerName});
+    let p = document.querySelector(`.name-${activePlayer}`).textContent;
+    let a = document.querySelector(`.name-${activePlayer == 0 ? 1 : 0}`).textContent;
+    console.log(p);
+    console.log(a);
+
+    if(winner == winnerName) {
+        changeLimit();
         document.querySelector(".slider").value = 0;
         playing = true;
         scores = [0, 0];
@@ -103,12 +144,14 @@ btnNew.addEventListener("click", function () {
     }
 });
 
-let presentPlayerScore, anotherPlayerScore;
+
 function slider() {
+    
     
     presentPlayerScore = document.querySelector(`#score--${activePlayer}`).textContent;
     anotherPlayerScore = document.querySelector(`#score--${activePlayer == 0 ? 1 : 0}`).textContent;
 
+    console.log({presentPlayerScore, anotherPlayerScore});
     if(presentPlayerScore >= winningScore){
         if(activePlayer == 0)
              document.querySelector(".slider").value = -winningScore;
@@ -134,17 +177,20 @@ function slider() {
 let nameOfPlayer, newInputPlayerName;
 
 for (let i = 0; i < 2; i++) {
-    document.querySelector(`.name-${i}`).addEventListener("dblclick", function (e) {
-        console.log(e);
+    document.querySelector(`.name-${i}`).addEventListener("dblclick", function () {
+        // console.log(e);
         nameOfPlayer = document.querySelector(`.name-${i}`).textContent;
-        console.log(nameOfPlayer);
+        // console.log(nameOfPlayer);
 
         document.querySelector(`.name-${i}`).classList.add("hidden");
         document.querySelector(`.changeName-${i}`).classList.remove("hidden");
         document.querySelector(`.changeName-${i}`).placeholder = nameOfPlayer;
         document.querySelector(`.changeName-${i}`).value = "";
 
-        document.querySelector(`.changeName-${i}`).addEventListener("keydown", function (e) {
+        document.querySelector(`.changeName-${i}`).addEventListener("keydown", changeName);
+        // document.addEventListener("click", changeName);
+        // document.querySelector(".newName").removeEventListener("click", changeName);
+        function changeName(e){
             if (e.key == "Enter") {
                     newInputPlayerName = document.querySelector(`.changeName-${i}`).value;
 
@@ -158,13 +204,22 @@ for (let i = 0; i < 2; i++) {
                     document.querySelector(`.playerName-${i}`).textContent  = nameOfPlayer;
                 }
             }
-        })
-
+        }
     });
 }
 
+function changeLimit(){
+    enterScore = Number(prompt("Enter your limit"));
+    if(enterScore != 0){
+        winningScore = enterScore;
+        document.querySelector(".slider").min = (0-winningScore);
+        document.querySelector(".slider").max = winningScore;
+        document.querySelector(".limitNumber").textContent = winningScore;
+    }
+}
 
+document.querySelector(".limitButton").addEventListener("click", changeLimit);
 
 // document.addEventListener("keydown", function(e){
 //     console.log(e);
-// });
+// })
